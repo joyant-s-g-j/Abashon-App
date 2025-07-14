86e129a185758c68ddba5e30c670cdf41eb51eb1
joyantgupta <joyantsheikharguptajoy@gmail.com>
2025-07-15 01:06:24 +0600

    tailwind setup

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,20 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className="text-red-600 font-bold text-2xl my-10">Welcome to Abashon</Text>
+      <Link href="/sign-in">Sign In</Link>
+      <Link href="/explore">explore</Link>
+      <Link href="./profile">profile</Link>
+      <Link href="./properties">Property</Link>
+    </View>
+  );
+}
