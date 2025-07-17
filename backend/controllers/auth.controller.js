136c8422b6a9f7d6b0847a8f96af8fe1fd750676
joyantgupta <joyantsheikharguptajoy@gmail.com>
2025-07-18 03:35:25 +0600

    singup controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,3 +1,4 @@
+import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 
 
@@ -37,6 +38,7 @@ export const signup = async (req, res) => {
             res.status(400).json({message: "Invalid user data" });
         }
     } catch (error) {
-        
+        console.log("Error in signup controller", error.message);
+        res.status(500).json({message: "Internal server error"});
     }
 }
